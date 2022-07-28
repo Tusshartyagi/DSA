b5f400651f9f0c1e4ca64c0feb6dd2772c3dab5b
Tusshar Tyagi <62403836+Tusshartyagi@users.noreply.github.com>
2022-07-28 23:52:43 +0530

    binary

---
+++ DSA/BinarySearch.js
@@ -0,0 +1,17 @@
+function searchAlgo(array,number){
+  let min=0;
+  let max=array.length-1;
+  while(min<=max){
+    let midIndex=Math.floor((min+max)/2);
+    if(array[midIndex]<number){
+      min = midIndex+1;
+    }else if(array[midIndex]>number){
+      max=midIndex-1;
+    }else{
+      return midIndex;
+    }
+  }
+  return -1;
+}
+const result = searchAlgo([1,2,3,4,5,6,7,8,9,10,11,12],8);
+console.log(result);
